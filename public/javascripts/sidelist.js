2002dc8f541e92e84edb54d699e0ed3ff39973b5
Jaewoong <jaewoong.here@gmail.com>
2015-07-15 22:02:21 +0900

    Fixing top button for mobile landscape view

--- public/javascripts/sidelist.js
+++ public/javascripts/sidelist.js
@@ -48,7 +48,7 @@ var attachSidelistAnimation = function() {
         window.scrollTo(0, 0);
     });
     window.addEventListener('scroll', function(topbutton) {
-        if (this.offsetParent === null) {
+        if (window.scrollY > this.offsetTop + this.offsetHeight) {
             topbutton.style.opacity = 1;
         } else {
             topbutton.style.opacity = 0;
